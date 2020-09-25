3d8a35707fb432f78ab9e38c9d2fd8398de063f3
deliriummary <deliriummary@gmail.com>
2020-09-25 12:30:26 -0700

    second commit

---
+++ build/precache-manifest.38c360c2f925a69bb6777c4118d9207a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c71ee7c3e6910b82dae9d18fd7afc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e47ef8306dedee60d3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d2f5d5f542ff092ae652",
+    "url": "/static/css/main.860d1de4.chunk.css"
+  },
+  {
+    "revision": "a5e47ef8306dedee60d3",
+    "url": "/static/js/2.6195e816.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.6195e816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f5d5f542ff092ae652",
+    "url": "/static/js/main.2cfc0541.chunk.js"
+  },
+  {
+    "revision": "ea049a9b5aa085bfefe9",
+    "url": "/static/js/runtime-main.fe0cf552.js"
+  },
+  {
+    "revision": "6e59b82eff6acd321b953ed86aa32085",
+    "url": "/static/media/navy.6e59b82e.jpg"
+  },
+  {
+    "revision": "2a2eebafa3ce968e5f8a75c24b1be777",
+    "url": "/static/media/navy2.2a2eebaf.jpg"
+  },
+  {
+    "revision": "d24d7cb069404b2704b55b01293ebe16",
+    "url": "/static/media/navy3.d24d7cb0.jpg"
+  }
+]);
